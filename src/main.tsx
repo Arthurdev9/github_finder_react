1255f54b9f8ba9ba71786878424636adf47162a2
Arthur Vinicius <arthurvinicius081@hotmail.com>
2025-07-22 15:01:22 -0300

    Criando a página dos repositórios e suas funcionalidades.

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx'
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './routes/Home.tsx'
+import Repos from './routes/Repos.tsx'
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Home />,
+      },
+      {
+        path: '/repos/:username',
+        element: <Repos />,
       },
     ],
   },
